b4f4e79951d0179bde99923792f0c9d3434d00f2
Doniyor <doniyorgit@gmail.com>
2025-11-12 15:08:18 +0500

    refactor: move linked-list files into a separate folder

--- index.ts
+++ index.ts
@@ -1,11 +1,1 @@
-import { LinkedList } from './src/data-structures/LinkedList';
-
-const linkedList = new LinkedList();
-linkedList.append(1);
-linkedList.append(2);
-linkedList.append(3);
-linkedList.append(4);
-linkedList.append(5);
-linkedList.reverse();
-
-console.log(linkedList.toArray());
+console.log('Hello Bun!');
