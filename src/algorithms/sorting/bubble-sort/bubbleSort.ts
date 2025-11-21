d4f2f19069728cc8f5aa80468414a8f214e23dab
Doniyor <doniyorgit@gmail.com>
2025-11-21 20:12:16 +0500

    feat: optimize bubbleSort the best case scenario

--- src/algorithms/sorting/bubble-sort/bubbleSort.ts
+++ src/algorithms/sorting/bubble-sort/bubbleSort.ts
@@ -1,9 +1,14 @@
-// inp: 5,11,6,3,4,7,3,2,6
-// out: 2,3,3,4,5,6,6,7,11
+// | Case                           | Complexity |
+// | ------------------------------ | ---------- |
+// | **Best case (already sorted)** | **O(n)**   |
+// | **Average case**               | **O(n²)**  |
+// | **Worst case**                 | **O(n²)**  |
 export function bubbleSort(arr: number[]): number[] {
     const sortedArr = [...arr];
 
     for (let i = sortedArr.length - 1; i >= 0; i--) {
+        let isSwapped = false;
+
         for (let j = 0; j < i; j++) {
             const a = sortedArr[j];
             const b = sortedArr[j + 1];
@@ -11,8 +16,11 @@ export function bubbleSort(arr: number[]): number[] {
             if (a > b) {
                 sortedArr[j] = b;
                 sortedArr[j + 1] = a;
+                isSwapped = true;
             }
         }
+
+        if (!isSwapped) break;
     }
 
     return sortedArr;
