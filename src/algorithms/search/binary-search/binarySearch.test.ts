4220710a895ea0a343236af77f5e26ffdbe5ddc2
Doniyor <doniyorgit@gmail.com>
2026-02-21 22:52:08 +0500

    feat: iterative binary-search algorithm

--- src/algorithms/search/binary-search/binarySearch.test.ts
+++ src/algorithms/search/binary-search/binarySearch.test.ts
@@ -1,13 +1,13 @@
-import { binarySearch } from './binarySearch';
+import { binarySearchRecursive } from "./binarySearch";
 
-describe('binarySearch', () => {
+describe("binarySearch", () => {
     const arr = [5, 2, 4, 6, 1, 3];
 
-    test('if element is in the arr, `true` is returned.', () => {
-        expect(binarySearch(arr, 4)).toBeTruthy();
+    test("if element is in the arr, `true` is returned.", () => {
+        expect(binarySearchRecursive(arr, 4)).toBeTruthy();
     });
 
-    test('if element is not in the arr, `false` is returned.', () => {
-        expect(binarySearch(arr, 7)).toBeFalsy();
+    test("if element is not in the arr, `false` is returned.", () => {
+        expect(binarySearchRecursive(arr, 7)).toBeFalsy();
     });
 });
--- src/algorithms/search/binary-search/binarySearch.ts
+++ src/algorithms/search/binary-search/binarySearch.ts
@@ -1,4 +1,7 @@
-export function binarySearch(sortedArr: number[], value: number): boolean {
+export function binarySearchRecursive(
+    sortedArr: number[],
+    value: number,
+): boolean {
     if (sortedArr.length <= 1) {
         return sortedArr[0] === value;
     }
@@ -6,8 +9,24 @@ export function binarySearch(sortedArr: number[], value: number): boolean {
     const midIndex = Math.floor(sortedArr.length / 2);
 
     if (value < sortedArr[midIndex]) {
-        return binarySearch(sortedArr.slice(0, midIndex), value);
+        return binarySearchRecursive(sortedArr.slice(0, midIndex), value);
     } else {
-        return binarySearch(sortedArr.slice(midIndex), value);
+        return binarySearchRecursive(sortedArr.slice(midIndex), value);
     }
+}
+
+export function binarySearch(arr: number[], target: number) {
+    let l = 0;
+    let r = arr.length - 1;
+
+    while (l < r) {
+        const m = Math.floor((l + r) / 2);
+
+        if (arr[m] === target) return m;
+
+        if (arr[m] < target) l = m + 1;
+        else r = m - 1;
+    }
+
+    return -1;
 }
