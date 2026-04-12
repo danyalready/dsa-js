2e0263d59d1b83e208774e3c4d430542cfc8227f
Doniyor <doniyorgit@gmail.com>
2026-04-12 11:06:28 +0500

    refacor: permutations without visited

--- src/algorithms/backtracking/permutations.ts
+++ src/algorithms/backtracking/permutations.ts
@@ -7,28 +7,20 @@ rl.on("line", (line: string) => {
 
     if (elements.length < 1 || 8 < elements.length) return;
 
-    const visited: boolean[] = new Array(elements.length).fill(false);
-
-    function backtrack(path: string[]) {
-        if (path.length === elements.length) {
-            console.log(path.join(" "));
+    function backtrack(start: number, elements: string[]) {
+        if (start === elements.length) {
+            console.log(elements.join(" "));
             return;
         }
 
-        for (let i = 0; i < elements.length; i++) {
-            if (visited[i]) continue;
-
-            visited[i] = true;
-
-            path.push(elements[i]);
-            backtrack(path);
-            path.pop();
-
-            visited[i] = false;
+        for (let i = start; i < elements.length; i++) {
+            [elements[start], elements[i]] = [elements[i], elements[start]];
+            backtrack(start + 1, elements);
+            [elements[start], elements[i]] = [elements[i], elements[start]];
         }
     }
 
-    backtrack([]);
+    backtrack(0, elements);
 
     rl.close();
 });
