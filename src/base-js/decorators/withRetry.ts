0352488ea3f1b243c9d58f2217b548bbe2e761c4
Doniyor <doniyor.otamurodov@gmail.com>
2026-06-18 11:36:28 +0500

    feat(base-js): With-retry decorator function

---
+++ src/base-js/decorators/withRetry.ts
@@ -0,0 +1,32 @@
+function withRetry<T extends (this: ThisParameterType<any>, ...args: any[]) => any>(
+    fn: T,
+    maxRetries: number,
+): (this: ThisParameterType<T>, ...args: Parameters<T>) => Promise<ReturnType<T>> {
+    return async function (this: ThisParameterType<T>, ...args: Parameters<T>) {
+        let lastError: unknown;
+
+        for (let i = 0; i < maxRetries; i++) {
+            try {
+                return await fn.apply(this, args);
+            } catch (error) {
+                lastError = error;
+            }
+        }
+
+        throw lastError;
+    };
+}
+
+const o = {
+    i: 0,
+    f: async function () {
+        if (this.i < 3) {
+            this.i++;
+            throw new Error();
+        }
+        return this.i;
+    },
+};
+
+const fooWithRetries = withRetry(o.f, 4);
+fooWithRetries.call(o).then(console.log);
