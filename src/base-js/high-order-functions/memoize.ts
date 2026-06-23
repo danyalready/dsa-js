1b6cdbfc1a5ad8887b5a80125da23cfade9fb1f5
Doniyor <doniyor.otamurodov@gmail.com>
2026-06-24 01:27:23 +0500

    feat(base-js): memoize high-order-function

---
+++ src/base-js/high-order-functions/memoize.ts
@@ -0,0 +1,30 @@
+function memoize<T extends (...params: number[]) => number>(
+    fn: T,
+): (...params: Parameters<T>) => ReturnType<T> {
+    const cache = new Map<string, any>();
+
+    function hash(...numbers: number[]) {
+        return numbers.join("-");
+    }
+
+    return function (this: ThisParameterType<T>, ...args: Parameters<T>) {
+        const key = hash(...args);
+
+        if (cache.has(key)) return cache.get(key);
+
+        const result = fn.apply(this, args);
+
+        cache.set(key, result);
+
+        return result;
+    };
+}
+
+let callCount = 0;
+const memoizedFn = memoize(function (a, b) {
+    callCount += 1;
+    return a + b;
+});
+memoizedFn(2, 3); // 5
+memoizedFn(2, 3); // 5
+console.log(callCount); // 1
